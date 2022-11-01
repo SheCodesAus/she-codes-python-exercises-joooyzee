88ec5dfae883f5d96c014d37d82e86d58cc0e2c4
Joy <65810856+joooyzee@users.noreply.github.com>
2022-11-01 19:36:50 +1100

    Update loops_playground.py

---
+++ loops/loops_playground.py
@@ -0,0 +1,57 @@
+# for loop
+# my_iterable =  [1, 2, 3, 4, 5, 6, 7, 8]
+
+# for item in my_iterable:
+#     # some code here
+#     print(item)
+
+# for num in range(1, 11):
+#     print(num)
+
+# for num in range(0,101):
+#     print(num)
+
+# for i in range(0,4):
+#     print('hello')
+
+
+# for letter in "Joy":
+#     print(letter)
+
+wishlist = ["igloo", "chicken", "donut toy", "cardboard box"]
+
+# for item in range(len(wishlist)):
+#     print(wishlist[item])
+
+# for item in wishlist:
+#     print(f"Chilli wants: {item}")
+
+# chilli_wishlist = [
+# ["igloo"],
+# ["donut toy", "tennis ball", "crocodile toy"],
+# ["chicken", "peanut butter"],
+# ["cardboard box", "kong", "dig mat"]
+# ]
+
+# for category in chilli_wishlist:
+#     for item in category:
+#         print(item)
+
+# WHILE LOOPS
+some_boolean_condition = False
+while some_boolean_condition:
+    # do something
+    pass
+
+# counter = 10
+# while counter <= 10:
+#     print(counter)
+#     # counter = counter + 1
+#     counter += 1
+
+# guess = ""
+# while guess != "a":
+#     guess = input("Guess a letter: ")
+
+
+
